831f3cadf842d92b24f985f46e5594af4505d7ce
Vionexy <dim.garb.dg@gmail.com>
2026-03-20 21:18:41 +0700

    refresh image cache by updated_at

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'schedule-v14';
+const CACHE = 'schedule-v15';
 const ROOT = new URL('./', self.location).pathname;
 const INDEX = new URL('./index.html', self.location).pathname;
 const MANIFEST = new URL('./manifest.json', self.location).pathname;
