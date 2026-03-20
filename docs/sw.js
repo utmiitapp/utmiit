81f1cb992643c6d6e9fbb59472c40805728d66b3
Vionexy <dim.garb.dg@gmail.com>
2026-03-20 20:59:02 +0700

    fix mini app cache for zoom

--- docs/sw.js
+++ docs/sw.js
@@ -1,13 +1,36 @@
-const CACHE = 'schedule-v13';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'schedule-v14';
+const ROOT = new URL('./', self.location).pathname;
+const INDEX = new URL('./index.html', self.location).pathname;
+const MANIFEST = new URL('./manifest.json', self.location).pathname;
+const FILES = [INDEX, MANIFEST];
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(INDEX, copy));
+        return res;
+      }).catch(() => caches.match(INDEX))
+    );
+    return;
+  }
+
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
